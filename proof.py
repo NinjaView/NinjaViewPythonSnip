ade024d5d10b77a56191d06eba3d8f5a0a39da85
NinjaView <148915340+NinjaView@users.noreply.github.com>
2023-10-29 14:20:58 -1000

    Update proof.py

--- proof.py
+++ proof.py
@@ -32,6 +32,19 @@ details = {
     "ngrokDOMAIN": "Default",
     "ngrokLOCATION": "Default"
 }
-base64_string = """R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="""
-image_data = base64.b64decode(base64_string)
-background_image = Image.open(BytesIO(image_data))
+
+def send_ati_command(command):
+    try:
+        # Connect to NinjaTrader ATI port
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('localhost', 36973))  # Adjust IP and port as necessary
+            s.sendall(command.encode())
+            data = s.recv(1024)
+            print('Received', repr(data))
+    except Exception as e:
+        print("An error occurred:", e)
+
+# Example command (replace with actual ATI command)
+command = "PLACE;Account=Sim1;Instrument=ES 12-23;Action=BUY;Qty=1;OrderType=LIMIT;LimitPrice=4139;TIF=DAY"
+send_ati_command(command)
+
